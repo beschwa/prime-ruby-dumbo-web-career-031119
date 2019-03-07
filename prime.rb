dfaae7a5e9bbf4774b396c3f04c6a61aa4f3e986
Michael Naraiswar <naraiswar@gmail.com>
2019-03-07 09:43:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,17 @@
 # Add  code here!
 def prime? (n)
-  is_prime = ""
+  is_prime = nil
   if n <= 3
     is_prime = true
-  elsif n % 2 == 0 || n % 3 == 0
-    is prime = false
   else
-    i = 5
-    while i * i <= n do
-      if n % i == 0 || n % (i + 2) == 0
+    (0..(n/2)).each do |i|
+      if n%i == 0
         is_prime = false
       end
-      i += 6
     end
-    is_prime = true
+    if is_prime != false
+      is_prime = true
+    end
   end
-  is_prime
+    is_prime
 end
